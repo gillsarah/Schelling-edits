560d6300cd8afb2f7c4bd47c711c6f88fae307f0
Sarah Gill <gills@uchicago.edu>
2019-04-25 12:29:01 -0500

    Update Schelling.py
    Role-back, re-try
    Just work on build_agents

--- Schelling.py
+++ Schelling.py
@@ -3,67 +3,40 @@ from numpy import random, mean
 """
 Homework 3: Modify the Agent Based Model from class.
 1. Add the following things to the params dict, and make the program use those
-   settings:
-   A boolean for whether or not to print results to the screen,
-   a boolean for whether or not to save results to file, and
-   a setting for the location to write the output to on your computer.
+   settings: A boolean for whether or not to print results to the screen, a
+   boolean for whether or not to save results to file, and a setting for the
+   location to write the output to on your computer.
 2. The current move method lets agents look through all vacant patches, and then
-   picks the first one that makes them happy.
-   Write a new move method, or modify the existing one, so that you have
-   the option of simply moving the agent to a random free location without
-   first checking happiness.
-   Add a parameter that switches between these two move processes.
+   picks the first one that makes them happy.  Write a new move method, or modify
+   the existing one, so that you have the option of simply moving the agent to a
+   random free location without first checking happiness.  Add a parameter that
+   switches between these two move processes.
 3. It is currently hard-coded that there are two types of agents, each of whom
    make up half of the population and have the same preference for similar
-   neighbors.
-   Change it so that the proportion of reds and blues is adjustable,
-   and each one should be able to have a different same_pref.
-   Change the report method to show results split by color.
+   neighbors.  Change it so that the proportion of reds and blues is adjustable,
+   and each one should be able to have a different same_pref.  Change the
+   report method to show results split by color.
 """
 
 
 #note: update out_path to point to somewhere on your computer
 params = {'world_size':(20,20),
           'num_agents':380,
-          'same_pref_r' :0.4,
-          'same_pref_b':0.7, #3 here, need to make variable? so one for r one for b?
+          'same_pref' :0.4,
           'max_iter'  :100,
-          'proportion_r': 0.3 #,#3? still need to add same_pref adjustability
-          #'out_path': (r'my path')} #1? plus some boulians?
-          }
+          'proportion_r': 0.5,
+          'out_path':r'~\Desktop\Programing\HW_3\output.csv'} #not sure where it's going, but not there!
 
 class Agent():
-    def __init__(self, world, kind, same_pref_r, same_pref_b):
+    def __init__(self, world, kind, same_pref):
         self.world = world
         self.kind = kind
-        self.same_pref_r = same_pref_r #3 here! do something here?
-        self.same_pref_b = same_pref_b
+        self.same_pref = same_pref
         self.location = None
 
     def move(self):
         #handle each agent's turn in the model iteration
         #returns 0 for happy, 1 for unhappy but moved, and 2 for unhappy and couldn't move
-
-#2 here! something here to add randomization of enter loop or move randomly w/o checking
-#vacancies = self.world.find_vacant(return_all=True)
-#move
-#do_i_like_it = self.am_i_happy()
-#    if do_i_like_it is True:
-#        return 1
-#    if do_i_like_it is False:
-#        return 2
-
-#from later in the code:
-       #finds all empty patches on the grid and returns a random one, unless kwarg return_all==True,
-        #then it returns a list of all empty patches
-
-#        empties = [loc for loc, occupant in self.grid.items() if occupant is None]
-#        if return_all:
-#            return empties
-#        else:
-#            choice_index = random.choice(range(len(empties)))
-#            return empties[choice_index]
-
         happy = self.am_i_happy()
 
         if not happy:
@@ -107,37 +80,11 @@ class Agent():
             return False
 
         perc_like_me = num_like_me / len(neighbor_kinds)
-#3 hard coded same_pref
-#now need to know agent's kind then can do the if statement:
-#this one is for red
-        if perc_like_me < self.same_pref_r:
-            return False
-        else:
-            return True
 
-'''
-added debug statements back in from class
-
-
-        #intermediat output to check on stuff:
         if perc_like_me < self.same_pref:
-            if not loc: #debug
-                print('Me:',self.kind) #debug
-                print('My neighbors:', neighbor_kinds) #debug
-                print('{} < {}'.format(perc_like_me, self.same_pref)) #debug
-                print('Not happy.') #debug
             return False
         else:
-            if not loc: #debug
-                print('Me:',self.kind) #debug
-                print('My neighbors:', neighbor_kinds) #debug
-                print('{} >= {}'.format(perc_like_me, self.same_pref)) #debug
-                print('Happy!') #debug
             return True
-
-end added debug statements
-'''
-
 
 class World():
     def __init__(self, params):
@@ -146,8 +93,7 @@ class World():
         self.reports = {}
 
         self.grid     = self.build_grid(  params['world_size'])
-        self.agents   = self.build_agents(params['num_agents'], params['same_pref_r'],
-                                          params['same_pref_b'], params['proportion_r'])
+        self.agents   = self.build_agents(params['num_agents'], params['same_pref'])
 
         self.init_world()
 
@@ -156,57 +102,24 @@ class World():
         locations = [(i,j) for i in range(world_size[0]) for j in range(world_size[1])]
         return {l:None for l in locations}
 
-    def build_agents(self, num_agents, same_pref_r, same_pref_b, proportion_r):
+    def build_agents(self, num_agents, same_pref):
         #generate a list of Agents that can be iterated over
 
-#3, hard code for 1/2 need to change the 2 to ref a parameter
-#'proportion_r_b': (0.5,0.5)
         def _kind_picker(i):
-            if i < round(num_agents * proportion_r):
+            if i < round(num_agents * params['proportion_r']):
                 return 'red'
             else:
                 return 'blue'
 
-'''
-check
-
-num_agents = 10
-proportion_r = 0.9
-my_test_agents = [_kind_picker(i) for i in range(num_agents)]
-my_test_agents
-
-check
-'''
-        def _pref_picker(i): #added a function so that agents can have diff prefs
-            if i < round(num_agents * proportion_r):
-                return same_pref_r
+        def _a_test(i):
+            if i < round(num_agents * params['proportion_r']):
+                return 0.1
             else:
-                return same_pref_b
+                return 0.2
 
-'''
-check
-
-same_pref_r = 0.3
-same_pref_b = 0.7
-my_test_pref = [_pref_picker(i) for i in range(num_agents)]
-my_test_pref
-'''
-
-'''
-Help, what does Agent mean in this context?
-'''
-                                                #changed so that calls my helper fn
-        agents = [Agent(self, _kind_picker(i), _pref_picker(i)) for i in range(num_agents)]
+        agents = [Agent(self, _kind_picker(i), _a_test(i)) for i in range(num_agents)]
         random.shuffle(agents)
         return agents
-
-'''check:fail
-agents = [Agent(_kind_picker(i), _pref_picker(i)) for i in range(num_agents)]
-agents
-build_agents()
-'''
-
-
 
     def init_world(self):
         #a method for all the steps necessary to create the starting point of the model
@@ -273,13 +186,13 @@ build_agents()
                     [not a for a in agent.am_i_happy(neighbor_check=True)]
                                 ))
         self.reports['integration'].append(round(mean(diff_neighbors), 2))
-#1???
+
     def run(self):
         #handle the iterations of the model
         log_of_happy = []
         log_of_moved = []
         log_of_stay  = []
-#this part is messed up by 2?
+
         self.report_integration()
         log_of_happy.append(sum([a.am_i_happy() for a in self.agents])) #starting happiness
         log_of_moved.append(0) #no one moved at startup
@@ -311,11 +224,11 @@ build_agents()
         self.reports['log_of_stay']  = log_of_stay
 
         self.report()
-#1
+
     def report(self, to_file=True):
         #report final results after run ends
         reports = self.reports
-#update bc 2?
+
         print('\nAll results begin at time=0 and go in order to the end.\n')
         print('The average number of neighbors an agent has not like them:', reports['integration'])
         print('The number of happy agents:', reports['log_of_happy'])
